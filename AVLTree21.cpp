aacf670b0c14603fdd79f5fb72f0297e5bc6a79a
kachmarPC <kachmar.developer@gmail.com>
2026-03-26 23:10:44 +0200

    add new file

---
+++ AVLTree21.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+
+    Node(int k) : key(k), left(nullptr), right(nullptr), height(1) {}
+};
+
+int getHeight(Node* node) {
+    return node ? node->height : 0;
+}
+
+int getBalance(Node* node) {
+    return node ? getHeight(node->left) - getHeight(node->right) : 0;
+}
+
+void updateHeight(Node* node) {
+    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+}
+
+Node* rotateRight(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    updateHeight(y);
+    updateHeight(x);
+
+    return x;
+}
+
+Node* rotateLeft(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    updateHeight(x);
+    updateHeight(y);
+
+    return y;
+}
+
+Node* insert(Node* node, int key) {
+    if (!node)
+        return new Node(key);
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+    updateHeight(node);
+
+    int balance = getBalance(node);
+
+    if (balance > 1 && key < node->left->key)
+        return rotateRight(node);
+
+    if (balance < -1 && key > node->right->key)
+        return rotateLeft(node);
+
+    if (balance > 1 && key > node->left->key) {
+        node->left = rotateLeft(node->left);
+        return rotateRight(node);
+    }
+
+    if (balance < -1 && key < node->right->key) {
+        node->right = rotateRight(node->right);
+        return rotateLeft(node);
+    }
+
+    return node;
+}
+
+int sumTree(Node* root) {
+    if (!root)
+        return 0;
+
+    return root->key + sumTree(root->left) + sumTree(root->right);
+}
+
+void inorder(Node* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->key << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = nullptr;
+
+    int arr[] = {10, 20, 30, 40, 50, 25};
+
+    for (int x : arr) {
+        root = insert(root, x);
+    }
+
+    cout << "In-order traversal: ";
+    inorder(root);
+    cout << endl;
+
+    cout << "Sum of all elements: " << sumTree(root) << endl;
+
+    return 0;
+}
