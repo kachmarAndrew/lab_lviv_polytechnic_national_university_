0e92c396cf8b83c910bf25ee806b26df523e9e83
kachmarPC <kachmar.developer@gmail.com>
2026-04-23 23:44:35 +0300

    add quick sort and change some logic in comparison

--- lab_8.cpp
+++ lab_8.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 using namespace chrono;
 
-/* 1. СОРТУВАННЯ ВИБОРОМ */
 void selectionSort(vector<int>& arr) {
     int n = arr.size();
 
@@ -23,14 +22,12 @@ void selectionSort(vector<int>& arr) {
     }
 }
 
-/* 2. КОКТЕЙЛЬНЕ СОРТУВАННЯ */
 void cocktailSort(vector<int>& arr) {
     bool swapped = true;
     int start = 0;
     int end = arr.size() - 1;
 
     while (swapped) {
-
         swapped = false;
 
         for (int i = start; i < end; i++) {
@@ -56,9 +53,7 @@ void cocktailSort(vector<int>& arr) {
     }
 }
 
-/* 3. СОРТУВАННЯ КОМІРКАМИ (Bucket Sort) */
 void bucketSort(vector<int>& arr) {
-
     int maxVal = *max_element(arr.begin(), arr.end());
     int bucketCount = 10;
 
@@ -73,16 +68,12 @@ void bucketSort(vector<int>& arr) {
 
     for (int i = 0; i < bucketCount; i++) {
         sort(buckets[i].begin(), buckets[i].end());
-
         for (int num : buckets[i])
             arr.push_back(num);
     }
 }
 
-/* 4. СОРТУВАННЯ ЗЛИТТЯМ */
-
 void merge(vector<int>& arr, int left, int mid, int right) {
-
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
@@ -97,55 +88,62 @@ void merge(vector<int>& arr, int left, int mid, int right) {
     int i = 0, j = 0, k = left;
 
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
     }
 
-    while (i < n1) {
-        arr[k] = L[i];
-        i++; k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++; k++;
-    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
 }
 
 void mergeSort(vector<int>& arr, int left, int right) {
-
     if (left < right) {
-
         int mid = left + (right - left) / 2;
 
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
-
         merge(arr, left, mid, right);
     }
 }
 
-/* ФУНКЦІЯ ДЛЯ ВИМІРЮВАННЯ ЧАСУ */
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void quickSort(vector<int>& arr) {
+    quickSort(arr, 0, arr.size() - 1);
+}
+
 long long testSort(void (*sortFunc)(vector<int>&), vector<int> arr) {
-
     auto start = high_resolution_clock::now();
-
     sortFunc(arr);
-
     auto stop = high_resolution_clock::now();
 
-    return duration_cast<milliseconds>(stop - start).count();
+    return duration_cast<nanoseconds>(stop - start).count();
 }
 
 int main() {
-
-    const int SIZE = 20000;
+    const int SIZE = 10000;
 
     vector<int> data(SIZE);
 
@@ -156,23 +154,21 @@ int main() {
     vector<int> arr2 = data;
     vector<int> arr3 = data;
     vector<int> arr4 = data;
+    vector<int> arr5 = data;
 
-    cout << "Selection Sort time: "
-         << testSort(selectionSort, arr1) << " ms\n";
-
-    cout << "Cocktail Sort time: "
-         << testSort(cocktailSort, arr2) << " ms\n";
-
-    cout << "Bucket Sort time: "
-         << testSort(bucketSort, arr3) << " ms\n";
+    cout << "Selection Sort time: " << testSort(selectionSort, arr1) << " ns\n";
+    cout << "Cocktail Sort time: " << testSort(cocktailSort, arr2) << " ns\n";
+    cout << "Bucket Sort time: " << testSort(bucketSort, arr3) << " ns\n";
 
     auto start = high_resolution_clock::now();
     mergeSort(arr4, 0, arr4.size() - 1);
     auto stop = high_resolution_clock::now();
 
     cout << "Merge Sort time: "
-         << duration_cast<milliseconds>(stop - start).count()
-         << " ms\n";
+         << duration_cast<nanoseconds>(stop - start).count()
+         << " ns\n";
+
+    cout << "Quick Sort time: " << testSort(quickSort, arr5) << " ns\n";
 
     return 0;
 }
